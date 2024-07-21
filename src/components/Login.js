218b99b02feb7487f2d1e9daeddc783464d7a13c
Ashraf Farag <farag.ashraf@icloud.com>
2024-07-21 10:34:17 +0300

    formicAndYup

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,8 +4,25 @@ import { InputText } from 'primereact/inputtext';
 import { Password } from 'primereact/password';
 import { useNavigate } from 'react-router-dom';
 import { authenticate } from '../api/authService';
-
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
+import { string } from 'yup';
 const Login = ({ onLogin }) => {
+
+    const formik = useFormik(
+        {
+            initialValues: {
+                username: '',
+                password: ''
+            },
+            validationSchema: Yup.object({
+                username: string().required("This field is required").min(4, "Length Must be 4 characters or less"),
+                password: string().required("This field is required")
+            })
+            ,
+            onSubmit: (values) => console.log(values)
+        }
+    )
     const [name, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [loading, setLoading] = useState(false);
@@ -38,7 +55,7 @@ const Login = ({ onLogin }) => {
 
     return (
         <>
-            <div className="flex justify-content-center align-items-center vh-100">
+            {/* <div className="flex justify-content-center align-items-center vh-100">
                 <div className="card p-5">
                     <h2 className="text-center mb-4">Login</h2>
                     {error && <div className="mb-3 text-danger">{error}</div>}
@@ -52,7 +69,44 @@ const Login = ({ onLogin }) => {
                     </div>
                     <Button label={loading ? "Signing in..." : "Login"} disabled={loading} icon="pi pi-check" onClick={handleLogin} className="w-full" />
                 </div>
-            </div>
+            </div> */}
+
+            <div className="flex justify-content-center align-items-center vh-100">
+                <div className="card p-5">
+                    <h2 className="text-center mb-4">Login</h2>
+                    <form onSubmit={formik.handleSubmit} >
+                        {error && <div className="mb-3 text-danger">{error}</div>}
+                        <div className="p-float-label  login-spaceBetween">
+                            <InputText
+                                id="username"
+                                name='username'
+                                placeholder='User Name'
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                                value={formik.values.username}
+                                disabled={loading}
+                                className="p-inputtext-lg" />
+                            <label htmlFor="username">User Name</label>
+                            {formik.touched.username && formik.errors.username ? <p className='red-alert'>{formik.errors.username}</p> : null}
+                        </div>
+                        <div className="p-float-label login-spaceBetween">
+                            <Password
+                                id="password"
+                                name='password'
+                                placeholder='User Name'
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                                value={formik.values.password}
+                                disabled={loading}
+                                toggleMask
+                                className="p-inputtext-lg" />
+                            <label htmlFor="password">Password</label>
+                            {formik.touched.password && formik.errors.password ? <p className='red-alert'>{formik.errors.password}</p> : null}
+                        </div>
+                        <Button type="submit" label={loading ? "Signing in..." : "Login"} disabled={loading} icon="pi pi-check" className="w-full" />
+                    </form>
+                </div>
+            </div >
         </>
     );
 };
